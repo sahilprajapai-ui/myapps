cfff510005403ca9895cdccf39fa54af58ab184a
sahilprajapati-ui <sahilprajapatix8@gmail.com>
2024-08-03 15:52:09 +0530

    minor fix

--- src/events.jsx
+++ src/events.jsx
@@ -1,5 +1,5 @@
 import EventCrd from "./eventCrd";
-import eve1img from "./images/ngo1.png"
+import eve1img from "./images/event image.png"
 import "./styles/event.css"
 
 
@@ -7,18 +7,14 @@ function Myevents() {
 
     const ngo1 = {
         img: eve1img ,
-        name: "Helpage India",
-        alt: "helpage india",
-        dd : "HelpAge India is a secular, not-for-profit organization in India, registered under the Societies’ Registration Act of 1860. Set up in 1978, "
-        
+        name: "Event title",
+        alt: "event title ",
+        dd : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, commodi!"       
     }
 
     return <div className="eventspage">
         <div className="title">Events</div>
         <div className="card-container">
-            <EventCrd {...ngo1} />
-            <EventCrd {...ngo1} />
-            <EventCrd {...ngo1} />
             <EventCrd {...ngo1} />
             <EventCrd {...ngo1} />
             <EventCrd {...ngo1} />
